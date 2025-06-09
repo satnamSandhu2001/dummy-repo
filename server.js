a9d3f4196b8ffd98932a8ff2b1668d1308e41744
Satnam Sandhu <satnamsandhu70002@gmail.com>
2025-06-09 13:39:42 +0530

    8

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.send('Hello World 8!');
 });
 
-setTimeout(() => {
-	app.listen(port, () => {
-		console.log(`Server is running at http://localhost:${port}`);
-	});
-}, 20 * 1000);
+app.listen(port, () => {
+	console.log(`Server is running at http://localhost:${port}`);
+
+	setTimeout(() => {
+		console.log('This message is delayed 10 seconds!');
+	}, 10 * 1000);
+});
